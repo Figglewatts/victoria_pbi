210ffc79fd9baf3cee42fa40cae2409c871a8d0e
Sam Gibson <sgibson@glasswallsolutions.com>
2019-11-05 13:43:25 +0000

    #100666: Add build pipelines

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ from setuptools import setup, find_packages
 setup(
     dependency_links=[],
     install_requires=[
-        "click==7.0", "marshmallow==3.2.1", "azure-devops==5.1.0b4"
+        'azure-devops==5.1.0b4', 'certifi==2019.9.11', 'chardet==3.0.4',
+        'click==7.0', 'colorama==0.4.1', 'idna==2.8', 'isodate==0.6.0',
+        'marshmallow==3.2.1', 'msrest==0.6.10', 'oauthlib==3.1.0',
+        'pyyaml==5.1.2', 'requests==2.22.0', 'requests-oauthlib==1.2.0',
+        'six==1.12.0', 'tabulate==0.8.5', 'urllib3==1.25.6', 'victoria==0.1'
     ],
     name="victoria_pbi",
     version="0.1",
